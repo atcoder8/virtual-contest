6dc03355178ea78be5298b935502fe9ca57a9627
atcoder8 <atcoder8@gmail.com>
2023-11-24 18:06:47 +0900

    Solved problem #7 in mayocorn-20231122.

--- mayocorn-20231122/src/bin/7.rs
+++ mayocorn-20231122/src/bin/7.rs
@@ -2,6 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-        
+        (n, x): (usize, usize),
+        aa: [usize; n],
     }
+
+    let mut less = 0;
+    let mut equal = 1;
+
+    for window in aa.windows(2) {
+        let rate = window[1] / window[0];
+        let d = x % window[1] / window[0];
+
+        (less, equal) = (
+            less + (d > 0) as usize * equal,
+            equal + (d < rate - 1) as usize * less,
+        );
+    }
+
+    println!("{}", less + equal);
 }
